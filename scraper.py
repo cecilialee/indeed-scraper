1eaa2bcb85682978208f76caaeffecfb04bd3061
Cecilia Lee <ceciliahslee@gmail.com>
2017-12-28 11:16:14 +0800

    Merge fieldnames with Xavier's scraper.

--- scraper.py
+++ scraper.py
@@ -27,11 +27,11 @@ import scraperwiki
 from bs4 import BeautifulSoup
 import requests as rqs
 from datetime import datetime
+import time
 import re
 import numpy as np
 from collections import defaultdict
 from collections import OrderedDict
-import pandas as pd
 import unicodecsv as csv
 
 
@@ -40,9 +40,11 @@ START_SCRAPING = True
 WHAT = "Data Scientist"
 WHERE = "Hong Kong"
 RECORD_CSV = True
-RECORD_EXCEL = True
+RECORD_EXCEL = False
 RECORD_DB = True
-FIELDNAMES = ['scrape_datetime', 'jk', 'job_title', 'company', 'job_location', 'post_datetime', 'job_description']
+# FIELDNAMES = ['scrape_datetime', 'jk', 'job_title', 'company', 'job_location', 'post_datetime', 'job_description']
+FIELDNAMES = ['epoch', 'scrping_dt', 'ad_id_indeed', 'ad_jobtitle_indeed', 'ad_cie_indeed', 'ad_jobloc_indeed', 'ad_post_dt_indeed', 'ad_jobdes_indeed', 'search_ad_url', 'ad_url', 'ad_jobdate', 'ad_jobtitle', 'ad_jobcie', 'ad_jobdes', 'ad_email']
+EXECTIME = datetime.now()
 # ========================================================
 
 
@@ -68,48 +70,74 @@ def scrape_single_page(what = WHAT, where = WHERE, start = 0):
 	# Initiate new dict to store *all* job data in one query.
 	page_job_data = defaultdict(str)
 
-	# Make BeautifulSoup with Indeed *job* page.
+	# Make Soup with Indeed *job* page.
 	job_soup = get_job_soup(what, where, start)
 
 	# Scrape every job data (expecting 15 results) on Indeed job page.
 	for job in job_soup.find_all('div', {'class': 'row'}):
 
-		# Initiate new dict to store a *single* job data.
-		# job_data = dict()
+		# Initiate new OrderedDict to store a *single* job data.
 		job_data = OrderedDict()
 
-		# `scrape_datetime`
-		scrape_datetime = datetime.now()
-		job_data['scrape_datetime'] = scrape_datetime
+		# `epoch`
+		job_data['epoch'] = time.mktime(EXECTIME.timetuple())
 
-		# `jk` : unique Indeed job id
+		# `scrping_dt`
+		job_data['scrping_dt'] = EXECTIME.strftime("%Y-%m-%d %H:%M:%S")
+
+		# `ad_id_indeed` : unique Indeed job id ('jk')
 		jk = job.attrs['data-jk']
-		job_data['jk'] = jk
+		job_data['ad_id_indeed'] = jk
 
-		# `job_title`
+		# `ad_jobtitle_indeed`
 		for content in job.find_all('a', {'data-tn-element': 'jobTitle'}):
-			job_data['job_title'] = content.text.lstrip()
+			job_data['ad_jobtitle_indeed'] = content.text.lstrip()
 
-		# `company`
-		# `job_location`
-		# `post_datetime`
-		for data in [['company', 'company'], ['location', 'job_location'], ['date', 'post_datetime']]:
-			for content in job.find_all('span', {'class': data[0]}):
-				job_data[data[1]] = content.text.lstrip()
+		# `ad_cie_indeed`
+		# `ad_jobloc_indeed`
+		# `ad_post_dt_indeed`
+		for field in [['company', 'ad_cie_indeed'], ['location', 'ad_jobloc_indeed'], ['date', 'ad_post_dt_indeed']]:
+			for content in job.find_all('span', {'class': field[0]}):
+				job_data[field[1]] = content.text.lstrip()
 
-		# TODO: wrangle `date` (str to datetime)
-		# TODO: `external_url`
+		# TODO: wrangle `ad_post_dt_indeed` (str to datetime)
 
-		# Make BeautifulSoup with Indeed *view job* page by passing the `jk` value.
+		# Make Soup with Indeed *view job* page by passing the jk value (`ad_id_indeed`).
 		viewjob_page = rqs.get(get_viewjob_url(jk))
 		viewjob_soup = BeautifulSoup(viewjob_page.text, 'html.parser')
 
-		# `job_description`
+		# `ad_jobdes_indeed`
 		for content in viewjob_soup.find_all('span', {'id': 'job_summary'}):
-			job_data['job_description'] = content.text
+			job_data['ad_jobdes_indeed'] = content.text
 
-		# Append single job data to `all_job_data` using `jk` as key.
-		page_job_data[jk] = job_data
+		# TODO: `ad_url`
+		# `search_ad_url`
+		# `ad_url`
+		for content in job.find_all('a', {'class': 'turnstileLink'}):
+			search_ad_url = 'https://www.indeed.hk' + content['href']
+			job_data['search_ad_url'] = search_ad_url
+
+			try:
+				ad_url = rqs.get(search_ad_url).url
+				job_data['ad_url'] = ad_url
+			except:
+				ad_url = search_ad_url
+				job_data['ad_url'] = search_ad_url
+
+
+		# Ad scraper data. Temporarily set to nan.
+		# `ad_jobdate`
+		# `ad_jobtitle`
+		# `ad_jobcie`
+		# `ad_jobdes`
+		# `ad_email`
+		for item in ['ad_jobdate', 'ad_jobtitle', 'ad_jobcie', 'ad_jobdes', 'ad_email']:
+			job_data[item] = np.nan
+
+		# Append single job data to `all_job_data` using `ad_url` as key.
+		page_job_data[ad_url] = job_data
+
+		print('Get {}'.format(ad_url))
 
 	return page_job_data
 
@@ -145,9 +173,9 @@ def scrape_all_pages(what = WHAT, where = WHERE):
 	all_job_data = defaultdict(str)
 
 	for i in start_range:
+		print("Scraping start = {0}.".format(i))
 		page_job_data = scrape_single_page(what, where, start = i)
 		all_job_data.update(page_job_data)
-		print("Scraping start = {0}.".format(i))
 
 	return all_job_data
 
@@ -165,7 +193,7 @@ def write_to_csv(filename, data):
 
 def write_to_db(data):
 	for value in data.values():
-		scraperwiki.sqlite.save(unique_keys = ['jk'], data = value)
+		scraperwiki.sqlite.save(unique_keys = ['ad_url'], data = value)
 
 
 
@@ -174,11 +202,10 @@ def scrape_indeed(what=WHAT, where=WHERE, record_csv=RECORD_CSV, record_excel=RE
 	print("Starting to scrape Indeed: {0} in {1}.".format(what, where))
 
 	data = scrape_all_pages(what, where)
-
-	date = datetime.now().strftime("%Y%m%d")
+	scrape_date = EXECTIME.strftime("%Y%m%d")
 
 	if record_csv:
-		csv_filename = '{0}_{1}_{2}_{3}.csv'.format(what, where, 'indeed', date).lower().replace(' ', '')
+		csv_filename = '{0}_{1}_{2}_{3}.csv'.format(what, where, 'indeed', scrape_date).lower().replace(' ', '')
 		print("Writing to '{0}'.".format(csv_filename))
 		write_to_csv(csv_filename, data)
 		print("Writing completed. View your data at '{0}'.".format(csv_filename))
